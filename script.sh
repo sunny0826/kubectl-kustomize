6e5c9ac78e042a0c10f3322bf33c2af76a480452
guoxudong <guoxudong@keking.cn>
2019-08-30 15:29:18 +0800

    fix scrip

--- script.sh
+++ script.sh
@@ -5,38 +5,43 @@ log(){
     echo "$*"
 }
 
-FILES=$(cat git.txt)
-FILES=${FILES//,/ }
-
 deploy(){
-    log "check config file: ${PLUGIN_CONFIG}/deploy/overlays/${PLUGIN_ENV}"
+    log "+ check config file: ${PLUGIN_CONFIG}/deploy/overlays/${PLUGIN_ENV}"
     cd ${PLUGIN_CONFIG}/deploy/overlays/${PLUGIN_ENV}
 
     if [ -z "${PLUGIN_TAG}" ]; then
-        log "set tag & image: ${PLUGIN_IMAGE}:${DRONE_TAG}"
+        log "+ set tag & image: ${PLUGIN_IMAGE}:${DRONE_TAG}"
         kustomize edit set image ${PLUGIN_IMAGE}:${DRONE_TAG}
     else
-        log "set image: ${PLUGIN_IMAGE}:${DRONE_BUILD_NUMBER}"
+        log "+ set image: ${PLUGIN_IMAGE}:${DRONE_BUILD_NUMBER}"
         kustomize edit set image ${PLUGIN_IMAGE}:${DRONE_BUILD_NUMBER}
     fi
 
-    log "deploy {${PLUGIN_NAME}} to {${PLUGIN_NAMESPACE}} timeout: ${PLUGIN_TIMEOUT}s"
+    log "+ deploy {${PLUGIN_NAME}} to {${PLUGIN_NAMESPACE}} timeout: ${PLUGIN_TIMEOUT}s"
     kubectl apply -k . && kubedog rollout track deployment ${PLUGIN_NAME} -n ${PLUGIN_NAMESPACE} -t ${PLUGIN_TIMEOUT}
 }
 
+FILES=$(cat git.txt)
+FILES=${FILES//,/ }
 IS_DEPLOY=false
 
-for element in $FILES
-do
-#    echo $element
-    if [ $element == ${PLUGIN_NAME} ]; then
-        IS_DEPLOY=true
-        break
-    fi
-done
+if ${PLUGIN_CHECK} ; then
+    for element in $FILES
+    do
+        if [ $element == ${PLUGIN_NAME} ]; then
+            IS_DEPLOY=true
+            break
+        fi
+    done
 
-if $IS_DEPLOY ; then
-    deploy
+    if $IS_DEPLOY ; then
+        deploy
+    else
+        log "+ skip module package deploy"
+    fi
+
 else
-    log "+ skip module package deploy"
+    log "+ skip check & start deploy"
+    deploy
 fi
+
